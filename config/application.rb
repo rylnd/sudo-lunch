0e18de4706cbfe608327c16161929bb821b198ff
Ryland Herrick <ryland@bendyworks.com>
2012-09-13 00:16:36 -0500

    Precompile assets on heroku

--- config/application.rb
+++ config/application.rb
@@ -74,5 +74,8 @@ module Bendylunch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent precompilation from blowing up on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
